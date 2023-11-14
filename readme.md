1ddd0ddb3e4335a99046ee2309ac99bcf4f881b5
SadlieRose <nazairesadlierose@gmail.com>
2023-11-14 15:17:55 -0500

    initialcommit

---
+++ readme.md
@@ -0,0 +1,5 @@
+// pour utilisation 
+1-importer le le package 
+    from cvsmergePack.cvsmerge import *
+
+2-
---
+++ test.py
@@ -0,0 +1,4 @@
+from cvsmergePack import fusionner_fichiers_csv
+
+# # Lancer la boucle principale de l'interface utilisateur
+fusionner_fichiers_csv()
